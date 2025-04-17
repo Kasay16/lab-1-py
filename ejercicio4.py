a6639281fed9b8d444a6a57330be22d5ba8a6ccc
Kasay16 <54045335+Kasay16@users.noreply.github.com>
2025-04-16 20:47:40 -0500

    ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,11 @@
+monto_cuenta = float(input("Ingrese el monto total de la cuenta: "))
+porcentaje_propina = float(input("Ingrese el porcentaje de propina que desea dejar: "))
+
+propina = monto_cuenta * (porcentaje_propina / 100)
+
+
+total = monto_cuenta + propina
+
+
+print(f"La propina es: {propina}")
+print(f"El total a pagar es: {total}")
